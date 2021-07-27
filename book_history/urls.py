bd8038269a8c3ef80b6ba22186ec0f4a4c122363
Fathulla <azizsavkatov@gmail.com>
2021-07-27 13:52:02 +0500

    new

--- book_history/urls.py
+++ book_history/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from book_history.views import HistoryListView
+from book_history.views import HistoryDetailView
 
 app_name = 'history'
 
 urlpatterns = [
-    path('', HistoryListView.as_view(), name="history")
+    path('<int:pk>/', HistoryDetailView.as_view(), name="history")
 ]
--- book_history/views.py
+++ book_history/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView
 
 from book_history.models import HistoryModel
 
 
-class HistoryListView(ListView):
+class HistoryDetailView(DetailView):
     template_name = 'history.html'
     queryset = HistoryModel.objects.order_by('-pk')
